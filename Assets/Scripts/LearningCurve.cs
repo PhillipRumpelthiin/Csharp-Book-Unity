ccf1c4f6f093215c89c12eaeb111bb6a8a2c9873
PhillipRumpelthiin <prumpe23@student.aau.dk>
2023-10-30 11:07:31 +0100

    Learning curve assignment

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,86 +4,32 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    //C-sharp book
-    public bool pureOfHeart = true;
-    public bool hasSecretIncantation = false;
-    public string rareItem = "Relic Stone";
-    public string CharacterAction = "Attack";
-
-    public int diceRoll = 7;
-
+    private int myNumber = 1;
+    public string myName = "Phillip Rumpelthiin";
 
     // Start is called before the first frame update
     void Start()
     {
-        OpenTreasureChamber();
-        PrintCharacterAction();
-        RollDice();
-        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
+        FindPartyMember(myName, myNumber);
+
+    }
+
+    public void FindPartyMember(string name, int number)
+    {
+        List<string> QuestPartyMembers = new List<string>()
         {
-            {"Potion", 5 },
-            {"Antidote", 7 },
-            {"Aspirin", 1 }
+            "Grim the Barbarian",
+            "Merlin the Wise",
+            "Sterling the Knight"
         };
+        QuestPartyMembers.Insert(number, name);
+        int listLength = QuestPartyMembers.Count; 
 
-        Debug.LogFormat("Items: {0}", itemInventory.Count);
-    }
+        Debug.LogFormat("Party Members: {0}", listLength);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void OpenTreasureChamber()
-    {
-        if (pureOfHeart && rareItem == "Relic Stone")
+        for (int i = 0; i < listLength; i++)
         {
-            if (!hasSecretIncantation)
-            {
-                Debug.Log("You have the spirit, but not the knowledge");
-            }
-            else
-            {
-                Debug.Log("The tresure is yours!");
-            }
-        }
-        else
-        {
-            Debug.Log("Come back when you have what it takes");
-        }
-    }
-
-    public void PrintCharacterAction()
-    {
-        switch (CharacterAction)
-        {
-            case "Heal":
-                Debug.Log("Potion sent.");
-                break;
-            case "Attack":
-                Debug.Log("To arms!");
-                break;
-            default:
-                Debug.Log("Shields up");
-                break;
-        }
-    }
-
-    public void RollDice()
-    {
-        switch (diceRoll)
-        {
-            case 7: 
-            case 15:
-                Debug.Log("Mediocre damage");
-                break;
-            case 20:
-                Debug.Log("Critical Hit!");
-                break;
-            default:
-                Debug.Log("Missed!");
-                break;
+            Debug.LogFormat("Index: {0} - {1}", i, QuestPartyMembers[i]);
 
         }
     }
