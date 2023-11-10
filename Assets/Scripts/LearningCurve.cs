c5091ec5b1c486c3217fdc63990abe9035d2faf8
PhillipRumpelthiin <prumpe23@student.aau.dk>
2023-11-10 14:25:47 +0100

    update

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,33 +4,18 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    private int myNumber = 1;
-    public string myName = "Phillip Rumpelthiin";
 
     // Start is called before the first frame update
     void Start()
     {
-        FindPartyMember(myName, myNumber);
-
-    }
-
-    public void FindPartyMember(string name, int number)
-    {
-        List<string> QuestPartyMembers = new List<string>()
+        Dictionary<string, int> itemInventory = new Dictionary<string, int>()
         {
-            "Grim the Barbarian",
-            "Merlin the Wise",
-            "Sterling the Knight"
+            {"Potion", 5 },
+            {"Antidote", 7 },
+            {"Aspirin", 1 }
         };
-        QuestPartyMembers.Insert(number, name);
-        int listLength = QuestPartyMembers.Count; 
 
-        Debug.LogFormat("Party Members: {0}", listLength);
-
-        for (int i = 0; i < listLength; i++)
-        {
-            Debug.LogFormat("Index: {0} - {1}", i, QuestPartyMembers[i]);
-
-        }
+        Debug.LogFormat("Items: {0} ", itemInventory.Count);
     }
+
 }
