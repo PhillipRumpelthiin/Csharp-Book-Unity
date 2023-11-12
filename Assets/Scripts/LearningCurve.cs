bc53426e0a144eb13e47b4b6625e12f1f6e0dfa5
PhillzR <rumpelthiin1997@gmail.com>
2023-11-12 13:40:39 +0100

    After reading chapter 4

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-
+    int heroGold = 5;
+    public int playerLives = 3; 
     // Start is called before the first frame update
     void Start()
     {
+        HealthStatus();
         Dictionary<string, int> itemInventory = new Dictionary<string, int>()
         {
             {"Potion", 5 },
@@ -15,7 +17,26 @@ public class LearningCurve : MonoBehaviour
             {"Aspirin", 1 }
         };
 
-        Debug.LogFormat("Items: {0} ", itemInventory.Count);
+        foreach (KeyValuePair<string, int> kvp in itemInventory)
+        {
+            Debug.LogFormat("Item: {0} - {1}g", kvp.Key, kvp.Value);
+
+            if (kvp.Value >= heroGold)
+            {
+                Debug.LogFormat("You can afford the  {0}", kvp.Key);
+            }
+        }
+
+
     }
 
+    public void HealthStatus()
+    {
+        while (playerLives > 0)
+        {
+            Debug.Log("Still alive!");
+            playerLives--;
+        }
+        Debug.Log("Player dead");
+    }
 }
